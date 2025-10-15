7fdc9e6666e06cc2d3d69ad5fa4c1b1881802d0d
nirajkr26 <nirajkumargupta2642006@gmail.com>
2025-10-15 11:46:22 +0530

    initial

---
+++ backend/src/app.js
@@ -0,0 +1,14 @@
+const express = require("express")
+
+const app = express();
+
+app.use((req, res) => {
+    res.send("hello from the server");
+})
+
+
+
+app.listen(3000, () => {
+    console.log("server running on port 3000")
+});
+
