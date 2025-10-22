3f3e7b0b28e43792a7979f993f73532fd24387ca
nirajkr26 <nirajkumargupta2642006@gmail.com>
2025-10-22 16:06:12 +0530

    pagination

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -53,6 +53,12 @@ router.get("/feed", userAuth, async (req, res) => {
     try {
         const loggedInUser = req.user;
 
+        const page = parseInt(req.query.page) || 1;
+        let limit = parseInt(req.query.limit) || 10;
+        limit = limit > 50 ? 50 : limit;
+
+        const skip = (page - 1) * limit;
+
         const connectionRequests = await ConnectionRequest.find({
             $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }]
         }).select("fromUserId toUserId");
@@ -69,7 +75,7 @@ router.get("/feed", userAuth, async (req, res) => {
                 { _id: { $nin: Array.from(hideUsersFromFeed) } },
                 { _id: { $ne: loggedInUser._id } }
             ]
-        }).select("firstName lastName age gender skills photoUrl");
+        }).select("firstName lastName age gender skills photoUrl").skip(skip).limit(limit);
 
         res.send(users);
 
